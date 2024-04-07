b71badd8dbdb93af75cc7d8fd64bd13004a6c5be
Diego-Batista <diegobtistadev@gmail.com>
2024-04-07 09:53:37 -0300

    Animando o OverlayFeedback

--- src/screens/Quiz/index.tsx
+++ src/screens/Quiz/index.tsx
@@ -45,6 +45,7 @@ export function Quiz() {
   const [currentQuestion, setCurrentQuestion] = useState(0);
   const [quiz, setQuiz] = useState<QuizProps>({} as QuizProps);
   const [alternativeSelected, setAlternativeSelected] = useState<null | number>(null);
+  const [statusReply, setStatusReply] = useState(0);
 
   const shake = useSharedValue(0);
   const scrollY = useSharedValue(0);
@@ -91,8 +92,11 @@ export function Quiz() {
     }
 
     if (quiz.questions[currentQuestion].correct === alternativeSelected) {
+      setStatusReply(1);
+
       setPoints(prevState => prevState + 1);
     } else {
+      setStatusReply(2);
       shakeAnimation();
     }
 
@@ -205,7 +209,7 @@ export function Quiz() {
 
   return (
     <View style={styles.container}>
-      <OverlayFeedback status={0} />
+       <OverlayFeedback status={statusReply} />
       <Animated.View
         style={fixedProgressBarStyles}
       >
