78f586875641d6d6a9c32046735fb2b547bfee84
Diego-Batista <diegobtistadev@gmail.com>
2024-04-11 14:45:14 -0300

    Animação das estrelas

--- src/components/Stars/styles.ts
+++ src/components/Stars/styles.ts
@@ -4,5 +4,11 @@ export const styles = StyleSheet.create({
   container: {
     justifyContent: 'center',
     alignItems: 'center'
+  },
+  canvas: {
+    width: 257,
+    height: 249,
+    position: 'absolute',
+    zIndex: 1
   }
 })
